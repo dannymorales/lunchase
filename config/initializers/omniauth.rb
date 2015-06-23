b5be89f0bba667997fc4067b452fa6beb1a3dbe7
dannymorales <danny@gadaproductions.com>
2015-06-22 23:53:41 -0400

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "5Lgkog6rZ5qm3k68gZrikfBfo", "3Gby1pVfUsSojp2wiuqaJV8CzfpLuYTPRt7Laq82P3HOLBiu1k"
-  # provider :facebook, "841331529253527", "ed2f3e49fd2f460dcad9857efb3cc362"
+  provider :twitter, "not for you", "not for you"
+  # provider :facebook, "not for you", "not for you"
 end
