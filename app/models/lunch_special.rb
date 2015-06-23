7c3ba45fc87ad294aed336907ee8fe73c3548f43
Danilo Morales <danny@gadaproductions.com>
2015-06-23 12:42:06 -0400

    added more stuff

--- app/models/lunch_special.rb
+++ app/models/lunch_special.rb
@@ -2,7 +2,7 @@ class LunchSpecial < ActiveRecord::Base
 	acts_as_votable
 	has_many :upvoted_users, through: :votes, source: :user
 	belongs_to :user
-	belongs_to :restaurants
+	belongs_to :restaurant
 	has_many :ingredients
 	# validates :restaurant_id, presence: true
 	validates :price, presence: true
